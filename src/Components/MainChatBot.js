b247fd3426d83b83ca6b5f53eae33476c74db0ce
ivanalame <alumno.399451@ies-azarquiel.es>
2023-11-23 19:22:12 +0100

    mejorasChatBot

--- src/Components/MainChatBot.js
+++ src/Components/MainChatBot.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import ChatBot from 'react-simple-chatbot'
-import { ThemeProvider } from 'styled-components'
+import React, { Component } from 'react';
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 
 const DiseñoChat = {
   background: '#f5f8fb',
@@ -14,8 +14,26 @@ const DiseñoChat = {
   userFontColor: '#4a4a4a',
 };
 
-
 export default class MainChatBot extends Component {
+  validarNombre = (value) => {
+    // Verificar si el nombre tiene más de 15 caracteres
+    if (value.length > 15) {
+      return 'El nombre debe tener máximo 15 caracteres.';
+    }
+
+    // Verificar si el nombre contiene números
+    if (/\d/.test(value)) {
+      return 'El nombre no puede contener números.';
+    }
+
+    // Verificar si la primera letra no está en mayúscula
+    if (value.charAt(0) !== value.charAt(0).toUpperCase()) {
+      return 'El nombre debe comenzar con mayúscula.';
+    }
+
+    return true;
+  };
+
   render() {
     return (
       <div>
@@ -23,28 +41,86 @@ export default class MainChatBot extends Component {
 
         <ThemeProvider theme={DiseñoChat}>
           <ChatBot
-          
             steps={[
               {
                 id: 'intro',
-                message: 'Bienvenido a mi ChatBot. ¿Como te llamas?',
-                trigger: '1', //con el trigger iremos al step con id nombre
+                message: 'Bienvenido a mi ChatBot. ¿Cómo te llamas?',
+                trigger: '1',
               },
               {
                 id: '1',
-                user: true, //con user permitimos el ingreso de datos por chat
-                trigger: '2', 
+                user: true,
+                validator: this.validarNombre,
+                trigger: '2',
               },
               {
                 id: '2',
-                // Usando {previousValue} pintaremos el valor recibido en el step anterior
-                message: 'Encantado de conocerte {previousValue}',     
-                trigger: '3',  
+                message: 'Encantado de conocerte {previousValue}',
+                trigger: '3',
               },
               {
                 id: '3',
-                message: '¿Que mas necesitas?',     
-                end: true,  
+                message: '¿Qué necesitas?',
+                trigger: '4',
+              },
+              {
+                id: '4',
+                options: [
+                  { value: 'y', label: 'Ayuda con React', trigger: 'ayudareact' },
+                  { value: 'n', label: 'Otros', trigger: 'comandosreact' },
+                  { value: 't', label: 'Nada mas', trigger: 'finCHAT' },
+                ],
+              },
+                  {
+                    id: 'ayudareact',
+                    message: "¿Que tipo de ayuda?",
+                    trigger: '5',
+                  },
+                  {
+                    id: '5',
+                    options:  [
+                      { value: '1', label: '¿Que es?', trigger: 'inicioreact' },
+                      { value: '2', label: 'Caracteristicas: JSX, ciclos de vida...', trigger: 'caracteristicasreact' },
+                      { value: '3', label: 'Hooks', trigger: 'hookreact' },
+                    ],
+                  },
+                  {
+                    id: 'inicioreact',
+                    message: 'Aqui tienes informacion sobre ¿Que es react?',
+                    trigger: '6',
+                  },
+                  {
+                    id: 'caracteristicasreact',
+                    message: 'Aqui tienes informacion sobre ¿Caracteristicas de react?',
+                    trigger: '6',
+                  },
+                  {
+                    id: 'hookreact',
+                    message: 'Aqui tienes informacion sobre ¿Hook de react?',
+                    trigger: '6',
+                  },
+
+                  {
+                    id: '6',
+                    message: '¿Necesitas Algo mas?',
+                    trigger: '7',
+                  },
+                  {
+                    id: '7',
+                    options:  [
+                      { value: 'yes', label: 'Si, necesito mas ayuda', trigger: '3' },
+                      { value: 'no', label: 'No gracias', trigger: 'finCHAT' },      
+                    ],
+                  },
+              {
+                id: 'comandosreact',
+                message: 'Ok',
+                end: true,
+              },
+              {
+                id: 'finCHAT',
+                message: "Estupendo,¡Ten un buen día!",
+                end: true,
               },
             ]}
           />
