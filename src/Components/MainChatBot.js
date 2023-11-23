0e9925c9cc320f96b7b6edf4e280f19a52ad6756
ivanalame <alumno.399451@ies-azarquiel.es>
2023-11-23 21:35:53 +0100

    Finish App

--- src/Components/MainChatBot.js
+++ src/Components/MainChatBot.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
+import WikiComponent from './WikiComponent';
 
 const DiseñoChat = {
   background: '#f5f8fb',
@@ -61,62 +62,69 @@ export default class MainChatBot extends Component {
               {
                 id: '3',
                 message: '¿Qué necesitas?',
-                trigger: '4',
+                trigger: 'select',
               },
               {
-                id: '4',
+                id: 'select',
                 options: [
                   { value: 'y', label: 'Ayuda con React', trigger: 'ayudareact' },
-                  { value: 'n', label: 'Otros', trigger: 'comandosreact' },
+                  { value: 'n', label: 'Aprender que es programacion', trigger: 'programacion' },
                   { value: 't', label: 'Nada mas', trigger: 'finCHAT' },
                 ],
               },
                   {
                     id: 'ayudareact',
                     message: "¿Que tipo de ayuda?",
-                    trigger: '5',
+                    trigger: 'selectReact',
                   },
                   {
-                    id: '5',
+                    id: 'selectReact',
                     options:  [
-                      { value: '1', label: '¿Que es?', trigger: 'inicioreact' },
-                      { value: '2', label: 'Caracteristicas: JSX, ciclos de vida...', trigger: 'caracteristicasreact' },
-                      { value: '3', label: 'Hooks', trigger: 'hookreact' },
+                      { value: 'Inicio', label: '¿Que es?', trigger: 'inicioreact' },
+                      { value: 'Características', label: 'Caracteristicas: JSX, ciclos de vida...', trigger: 'caracteristicasreact' },
+                      { value: 'Hooks', label: 'Hooks', trigger: 'hookreact' },
                     ],
                   },
                   {
                     id: 'inicioreact',
                     message: 'Aqui tienes informacion sobre ¿Que es react?',
-                    trigger: '6',
-                  },
-                  {
-                    id: 'caracteristicasreact',
-                    message: 'Aqui tienes informacion sobre ¿Caracteristicas de react?',
-                    trigger: '6',
-                  },
-                  {
-                    id: 'hookreact',
-                    message: 'Aqui tienes informacion sobre ¿Hook de react?',
-                    trigger: '6',
-                  },
-
-                  {
-                    id: '6',
-                    message: '¿Necesitas Algo mas?',
                     trigger: '7',
                   },
                   {
-                    id: '7',
+                    id: 'caracteristicasreact',
+                    message: 'Aqui tienes informacion sobre Caracteristicas de react',
+                    trigger: '7',
+                  },
+                  {
+                    id: 'hookreact',
+                    message: 'Aqui tienes informacion sobre Hook de react',
+                    trigger: '7',
+                  },
+
+                  {
+                    id: 'programacion',
+                    message: '¡Claro aqui tienes un enlace donde ver que es la programacion!',
+                    trigger: '7',
+                  },
+                  {
+                 
+                    id: "7",
+                    component: <WikiComponent />,
+                    asMessage: true,
+                    trigger: "preguntaVuelta"
+                 },
+                  {
+                    id: 'preguntaVuelta',
+                    message: '¿Necesitas Algo mas?',
+                    trigger: '8',
+                  },
+                  {
+                    id: '8',
                     options:  [
                       { value: 'yes', label: 'Si, necesito mas ayuda', trigger: '3' },
                       { value: 'no', label: 'No gracias', trigger: 'finCHAT' },      
                     ],
                   },
-              {
-                id: 'comandosreact',
-                message: 'Ok',
-                end: true,
-              },
               {
                 id: 'finCHAT',
                 message: "Estupendo,¡Ten un buen día!",
